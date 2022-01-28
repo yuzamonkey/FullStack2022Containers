3207320dcb7bb206e6420dc4dfc47628a113a960
Jusa Mankki <jusamankki@gmail.com>
2022-01-28 09:35:28 +0200

    exc19

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,10 +1,6 @@
 const MONGO_URL = process.env.MONGO_URL || undefined
 const REDIS_URL = process.env.REDIS_URL || undefined
 
-console.log("\n•••\nMONGO_URL", MONGO_URL)
-console.log("REDIS_URL", REDIS_URL)
-console.log("PORT", process.env.PORT, "\n•••\n")
-
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
   REDIS_URL//: '//localhost:6378'
